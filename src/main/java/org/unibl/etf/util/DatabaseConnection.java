b749f6404b4347aa2daa186ec5d6c6ea5b230bf5
marko-cerovac <marko.cerovac16@gmail.com>
2026-03-23 07:53:43 +0100

    created the DatabaseConnection singleton

---
+++ src/main/java/org/unibl/etf/util/DatabaseConnection.java
@@ -0,0 +1,49 @@
+package org.unibl.etf.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * DatabaseConnection
+ */
+public final class DatabaseConnection {
+    private static volatile DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection() throws SQLException {
+        Properties properties = new Properties();
+
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Unable to find db.properties in resources");
+            }
+            properties.load(input);
+
+            String url      = properties.getProperty("db.url");
+            String username = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
+
+            this.connection = DriverManager.getConnection(url, username, password);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading database configuration file", e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static synchronized DatabaseConnection getInstance() throws SQLException {
+        if (instance == null || instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+
+        return instance;
+    }
+    
+}
