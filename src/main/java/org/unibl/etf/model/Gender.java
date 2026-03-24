084e98bd21be58bbb0666a6c1f56676748140198
marko-cerovac <marko.cerovac16@gmail.com>
2026-03-24 11:38:58 +0100

    added toString to the Gender enum

--- src/main/java/org/unibl/etf/model/Gender.java
+++ src/main/java/org/unibl/etf/model/Gender.java
@@ -3,5 +3,14 @@ package org.unibl.etf.model;
 public enum Gender {
     Male,
     Female,
-    AlphabetPeople,
+    AlphabetPeople;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case Male -> "Male";
+            case Female -> "Female";
+            case AlphabetPeople -> "AlphabetPeople";
+        };
+    }
 }
